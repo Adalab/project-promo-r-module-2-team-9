902264865bbb2d7579a9de2ff044349e3f828bdd
Bella Irene <bellagarcia1985@gmail.com>
2022-10-24 16:37:34 +0200

    reset js

--- src/js/0_variables.js
+++ src/js/0_variables.js
@@ -8,11 +8,5 @@ const jobInput = document.querySelector ('.js_input-job');
 const emailInput = document.querySelector ('.js_input-email');
 const tlfInput = document.querySelector ('.js_input-tlf');
 const linInput = document.querySelector ('.js_input-lin');
-const githupInput = document.querySelector ('.input-githup');
+const githupInput = document.querySelector ('.js_input-githup');
 
-resetBtn.addEventListener ('click', (event) => {
-event.preventDefault();
-
-
-
-})
