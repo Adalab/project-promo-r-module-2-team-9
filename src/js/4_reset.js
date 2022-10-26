9e98b4f483a10391e31eee37ea11ab87bc8533c5
Bella Irene <bellagarcia1985@gmail.com>
2022-10-26 16:43:14 +0200

    reset dual function

--- src/js/4_reset.js
+++ src/js/4_reset.js
@@ -9,6 +9,14 @@ resetBtn.addEventListener ('click', (event) => {
      tlfInput.value = '';
      linInput.value = '';
     githupInput.value = '';
+    if (nameInput.value === '') {
+        previewName.innerHTML = 'Nombre Apellidos';
+    }
+    if (jobInput.value === '') {
+        previewJob.innerHTML = 'Front-end developer';
+    }
        
        });
 
+    //    // primero le digo que si le doy a reset me ponga todas las casillas vacias
+    //    y luego que si name esta vacio que me escriba nombre apellidos y lo mismo con job
