fc09c72fe2715539b2f2c6678419926cb2b9c87a
Lara <lrpoblet@gmail.com>
2022-11-17 20:46:11 +0100

    fix issue in create.html

--- src/js/6_create.js
+++ src/js/6_create.js
@@ -18,13 +18,11 @@ function saveInfo() {
   })
     .then((response) => response.json())
     .then((responseJSON) => {
-      console.log(responseJSON);
       let data = responseJSON.cardURL;
       link.innerHTML = data;
       link.href = data;
       twitter.href = `https://twitter.com/intent/tweet?text=Aquí%20podéis%20ver%20mi%20tarjeta%20virtual&url=${data}.%20Crea%20la%20tuya%20en:%20http://beta.adalab.es/project-promo-r-module-2-team-9/`;
       localStorage.setItem('userData', JSON.stringify(dataCard));
-      console.log(dataCard);
     });
 }
 
